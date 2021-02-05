2cd04c343af30138e23958af54e1a8e4af742709
scotth527 <scottyh527@gmail.com>
2021-02-05 17:50:42 -0500

    Tested pages, tried out static pages

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from django.views import generic
 # Create your views here.
 from .models import Question, Choice
+from django.utils import timezone
 
 # Generics cannot change the name of the variable names
 class IndexView(generic.ListView):
@@ -13,11 +14,18 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        # pub_date__lte means less than or equal to, today
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+            """
+            Excludes any questions that aren't published yet.
+            """
+            return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -40,3 +48,5 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
